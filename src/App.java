95d7efa4275943d8a5cbba0a5aff637177571e25
jmnote <opcore@gmail.com>
2022-12-24 17:08:13 +0900

    first commit

---
+++ src/App.java
@@ -0,0 +1,12 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+import com.google.gson.Gson;
+
+public class App {
+    public static void main(String[] args) {
+        Gson gson = new Gson();
+        System.out.println("hello");
+        System.out.println(gson);
+    }
+}
